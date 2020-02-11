74028af3a6bb45a87e303385aa1d410bd31044e8
Gu234 <guciofilipek@gmail.com>
2020-02-11 16:54:42 +0100

    Updates

---
+++ precache-manifest.4a66512bfe14dc2cf7679e9ce9ae90ab.js
@@ -0,0 +1,442 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563efe585092b18c4f073815d368ee4e",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "7d9ec3d27e4c905eb958",
+    "url": "/memory-game/static/css/2.0d654ebb.chunk.css"
+  },
+  {
+    "revision": "ecafcd630b7f180fc240",
+    "url": "/memory-game/static/css/main.eb787fd8.chunk.css"
+  },
+  {
+    "revision": "7d9ec3d27e4c905eb958",
+    "url": "/memory-game/static/js/2.8b488b7b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory-game/static/js/2.8b488b7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecafcd630b7f180fc240",
+    "url": "/memory-game/static/js/main.2ec12a5e.chunk.js"
+  },
+  {
+    "revision": "2fc23ae0a74c14a4fedf",
+    "url": "/memory-game/static/js/runtime-main.a4d8dd60.js"
+  },
+  {
+    "revision": "0292ec4ebac41c7e4107e34474814eaa",
+    "url": "/memory-game/static/media/amazonwebservices-original.0292ec4e.svg"
+  },
+  {
+    "revision": "0cfaad50e21b262d9074b6662915de3a",
+    "url": "/memory-game/static/media/android-original.0cfaad50.svg"
+  },
+  {
+    "revision": "db12f97bda8cd0682f2c1daa1396daa3",
+    "url": "/memory-game/static/media/angularjs-original.db12f97b.svg"
+  },
+  {
+    "revision": "01d7be02a868837e449ca2b956cc0875",
+    "url": "/memory-game/static/media/apache-original.01d7be02.svg"
+  },
+  {
+    "revision": "6900d7ccd36be78c0525e21ec346e7f8",
+    "url": "/memory-game/static/media/appcelerator-original.6900d7cc.svg"
+  },
+  {
+    "revision": "d53e36c55ebff4c48f3b797b0d7602fe",
+    "url": "/memory-game/static/media/apple-original.d53e36c5.svg"
+  },
+  {
+    "revision": "7ce634bb90809b8d4eb25a19b3fc5e9d",
+    "url": "/memory-game/static/media/atom-original.7ce634bb.svg"
+  },
+  {
+    "revision": "529bf9cdff934f9ce5f709e3c2e9073d",
+    "url": "/memory-game/static/media/babel-original.529bf9cd.svg"
+  },
+  {
+    "revision": "39eac6f48bf88a110cf4a56186a48c00",
+    "url": "/memory-game/static/media/backbonejs-original.39eac6f4.svg"
+  },
+  {
+    "revision": "e73f76fdc4922dfc04ffbca0c8dc2b0d",
+    "url": "/memory-game/static/media/behance-original.e73f76fd.svg"
+  },
+  {
+    "revision": "edcc6a50f345fb3fbc4ebea643c04db6",
+    "url": "/memory-game/static/media/bitbucket-original.edcc6a50.svg"
+  },
+  {
+    "revision": "29d9c4c65bdce3909db835e6abaf4685",
+    "url": "/memory-game/static/media/bower-original.29d9c4c6.svg"
+  },
+  {
+    "revision": "3505d536922da9f20d0ce457cfb7775a",
+    "url": "/memory-game/static/media/c-original.3505d536.svg"
+  },
+  {
+    "revision": "1effe9aa339eb9461941ac8322e4f3ba",
+    "url": "/memory-game/static/media/cakephp-original.1effe9aa.svg"
+  },
+  {
+    "revision": "1d83444c3f0edc4e9b65bda74a722e5d",
+    "url": "/memory-game/static/media/ceylon-original.1d83444c.svg"
+  },
+  {
+    "revision": "1972999cb134c768168fc8cf3db949f6",
+    "url": "/memory-game/static/media/chrome-original.1972999c.svg"
+  },
+  {
+    "revision": "564f474fba22eb0fa9b67da2d3a4b701",
+    "url": "/memory-game/static/media/clojure-original.564f474f.svg"
+  },
+  {
+    "revision": "7724577f5b7b68d8a2559ca5d4870038",
+    "url": "/memory-game/static/media/clojurescript-original.7724577f.svg"
+  },
+  {
+    "revision": "54823951927f895e1d080a0955cd96b1",
+    "url": "/memory-game/static/media/coffeescript-original.54823951.svg"
+  },
+  {
+    "revision": "bae982aa8b8422b37ff9977af10eadf2",
+    "url": "/memory-game/static/media/confluence-original.bae982aa.svg"
+  },
+  {
+    "revision": "9deb93976aec63450efd04b3e97d655e",
+    "url": "/memory-game/static/media/couchdb-original.9deb9397.svg"
+  },
+  {
+    "revision": "a5d9bc3d587f95ced9864a8481979bcd",
+    "url": "/memory-game/static/media/cplusplus-original.a5d9bc3d.svg"
+  },
+  {
+    "revision": "c6961b79062b431cb39106d60635832d",
+    "url": "/memory-game/static/media/csharp-original.c6961b79.svg"
+  },
+  {
+    "revision": "a70a7e3e545f3ddc5707d4c4da8cfa17",
+    "url": "/memory-game/static/media/css3-original.a70a7e3e.svg"
+  },
+  {
+    "revision": "bd70f32d9ff43e4ee3be96607cb6a210",
+    "url": "/memory-game/static/media/d3js-original.bd70f32d.svg"
+  },
+  {
+    "revision": "0e2362b1bf9b9763b9ae0b7f538ebe89",
+    "url": "/memory-game/static/media/debian-original.0e2362b1.svg"
+  },
+  {
+    "revision": "8ba16e944b2ded1df7d2c447a5c1f3a4",
+    "url": "/memory-game/static/media/devicon-original.8ba16e94.svg"
+  },
+  {
+    "revision": "792f835aac1501f579814406af2e0eae",
+    "url": "/memory-game/static/media/django-original.792f835a.svg"
+  },
+  {
+    "revision": "e618708b41bf4b0b9ee2720423e7a242",
+    "url": "/memory-game/static/media/docker-original.e618708b.svg"
+  },
+  {
+    "revision": "de8db92e3a8885e2c908941cc33dd48b",
+    "url": "/memory-game/static/media/doctrine-original.de8db92e.svg"
+  },
+  {
+    "revision": "01af77e16f2040c3443eecc84a91a85c",
+    "url": "/memory-game/static/media/dot-net-original.01af77e1.svg"
+  },
+  {
+    "revision": "38476b6af86ba177c23b24cc228e7c8f",
+    "url": "/memory-game/static/media/drupal-original.38476b6a.svg"
+  },
+  {
+    "revision": "e903b609335052437cac1ae5403160e9",
+    "url": "/memory-game/static/media/electron-original.e903b609.svg"
+  },
+  {
+    "revision": "11544098ff3399a9f156291621c60017",
+    "url": "/memory-game/static/media/elm-original.11544098.svg"
+  },
+  {
+    "revision": "e0cfde3b4d6633940636092e7bbef4c3",
+    "url": "/memory-game/static/media/erlang-original.e0cfde3b.svg"
+  },
+  {
+    "revision": "c3af8b67d896ace3528c7f109750e875",
+    "url": "/memory-game/static/media/express-original.c3af8b67.svg"
+  },
+  {
+    "revision": "944586581a185c505df885cabd16706e",
+    "url": "/memory-game/static/media/facebook-original.94458658.svg"
+  },
+  {
+    "revision": "0c2536f99a9c847013dfcafa3fb6c0a9",
+    "url": "/memory-game/static/media/firefox-original.0c2536f9.svg"
+  },
+  {
+    "revision": "756df9e92a524c6f5a273f1704fe20f7",
+    "url": "/memory-game/static/media/foundation-original.756df9e9.svg"
+  },
+  {
+    "revision": "eae82bebf5f3d96f3872e71ac41fec9c",
+    "url": "/memory-game/static/media/gimp-original.eae82beb.svg"
+  },
+  {
+    "revision": "c02b7da93fea7f472b89583e23f12759",
+    "url": "/memory-game/static/media/git-original.c02b7da9.svg"
+  },
+  {
+    "revision": "4097a5be85bf4c4e64ec52f389ea039d",
+    "url": "/memory-game/static/media/github-original.4097a5be.svg"
+  },
+  {
+    "revision": "80922f3d5f71dc3e4723b37ffa538afa",
+    "url": "/memory-game/static/media/gitlab-original.80922f3d.svg"
+  },
+  {
+    "revision": "74c1374cfabfb69f55a5377482ab5852",
+    "url": "/memory-game/static/media/go-original.74c1374c.svg"
+  },
+  {
+    "revision": "e0ca0a35192aff62182a918563894307",
+    "url": "/memory-game/static/media/google-original.e0ca0a35.svg"
+  },
+  {
+    "revision": "bf05268b34fdd6b846e676d220ba058e",
+    "url": "/memory-game/static/media/grunt-original.bf05268b.svg"
+  },
+  {
+    "revision": "6954cf00013373c5684ff940cf9d70e3",
+    "url": "/memory-game/static/media/handlebars-original.6954cf00.svg"
+  },
+  {
+    "revision": "3af582e56c213a6439e38c3f360bd633",
+    "url": "/memory-game/static/media/heroku-original.3af582e5.svg"
+  },
+  {
+    "revision": "7997c4244e7a8cf2b160803e1c741502",
+    "url": "/memory-game/static/media/html5-original.7997c424.svg"
+  },
+  {
+    "revision": "f2779d40f6f80060d355807ac885e91b",
+    "url": "/memory-game/static/media/ie10-original.f2779d40.svg"
+  },
+  {
+    "revision": "b2ed4de79110ca4eaf224d43de73241e",
+    "url": "/memory-game/static/media/inkscape-original.b2ed4de7.svg"
+  },
+  {
+    "revision": "357626e4c1d133bc4185c3eab5849d91",
+    "url": "/memory-game/static/media/intellij-original.357626e4.svg"
+  },
+  {
+    "revision": "241f2cea8366ed66e2d161019d06a147",
+    "url": "/memory-game/static/media/ionic-original.241f2cea.svg"
+  },
+  {
+    "revision": "66517ded2b3a6bef72a9c7fc9a1fc931",
+    "url": "/memory-game/static/media/java-original.66517ded.svg"
+  },
+  {
+    "revision": "de79c8f14c2b0d692ae215d7e6eccd54",
+    "url": "/memory-game/static/media/javascript-original.de79c8f1.svg"
+  },
+  {
+    "revision": "75db59a97f36be54ae36399a2f64becf",
+    "url": "/memory-game/static/media/jeet-original.75db59a9.svg"
+  },
+  {
+    "revision": "3a8572b347f48b346c867d1fa4c7a712",
+    "url": "/memory-game/static/media/jetbrains-original.3a8572b3.svg"
+  },
+  {
+    "revision": "cebb17c0855c821f6f042fc2956b54e8",
+    "url": "/memory-game/static/media/jquery-original.cebb17c0.svg"
+  },
+  {
+    "revision": "f0746bc8e4e004b9e867313e5fd6380b",
+    "url": "/memory-game/static/media/krakenjs-original.f0746bc8.svg"
+  },
+  {
+    "revision": "bad4685f20f2ef1a53f6f7d6486ad8af",
+    "url": "/memory-game/static/media/linkedin-original.bad4685f.svg"
+  },
+  {
+    "revision": "2bf747f09a4f9135fbd3032432f4158b",
+    "url": "/memory-game/static/media/linux-original.2bf747f0.svg"
+  },
+  {
+    "revision": "5b0205da9b32941104b606c56c83ee91",
+    "url": "/memory-game/static/media/meteor-original.5b0205da.svg"
+  },
+  {
+    "revision": "52783a2f8043e237279093badadc9cf1",
+    "url": "/memory-game/static/media/mongodb-original.52783a2f.svg"
+  },
+  {
+    "revision": "0763f2492d17c139e43c5bcea423d9c7",
+    "url": "/memory-game/static/media/moodle-original.0763f249.svg"
+  },
+  {
+    "revision": "c326c80ab2f4dc4288ece5e55e746cbe",
+    "url": "/memory-game/static/media/mysql-original.c326c80a.svg"
+  },
+  {
+    "revision": "9bb21db07e19c2a7a5c0712bfb1127bd",
+    "url": "/memory-game/static/media/nginx-original.9bb21db0.svg"
+  },
+  {
+    "revision": "ccb43d3400fb05951c89ad3ba6acd9d5",
+    "url": "/memory-game/static/media/nodejs-original.ccb43d34.svg"
+  },
+  {
+    "revision": "429c7789e8e5bbbdfdefaf2534536337",
+    "url": "/memory-game/static/media/nodewebkit-original.429c7789.svg"
+  },
+  {
+    "revision": "369bae91ff659183c379c49cc099660b",
+    "url": "/memory-game/static/media/oracle-original.369bae91.svg"
+  },
+  {
+    "revision": "7be7348ea07780291787cb3825fbf59f",
+    "url": "/memory-game/static/media/php-original.7be7348e.svg"
+  },
+  {
+    "revision": "2f9bcccf4e5bade49b931b6682593045",
+    "url": "/memory-game/static/media/phpstorm-original.2f9bcccf.svg"
+  },
+  {
+    "revision": "1e83f7103d15e32357b2dc768106fac0",
+    "url": "/memory-game/static/media/postgresql-original.1e83f710.svg"
+  },
+  {
+    "revision": "c65b5a946a3b31ee10d467fd78f56c45",
+    "url": "/memory-game/static/media/pycharm-original.c65b5a94.svg"
+  },
+  {
+    "revision": "3b84eb4026f0c704e7e3d16972bb0e3d",
+    "url": "/memory-game/static/media/python-original.3b84eb40.svg"
+  },
+  {
+    "revision": "e9f3d01c242e086f895bde131803b451",
+    "url": "/memory-game/static/media/react-original.e9f3d01c.svg"
+  },
+  {
+    "revision": "a4f78391c9e162046bc96db6dd6d516d",
+    "url": "/memory-game/static/media/redhat-original.a4f78391.svg"
+  },
+  {
+    "revision": "8cd965803d2667506d2baf9b56122aec",
+    "url": "/memory-game/static/media/redis-original.8cd96580.svg"
+  },
+  {
+    "revision": "b7dc3b99382465652bda0e61515c0937",
+    "url": "/memory-game/static/media/redux-original.b7dc3b99.svg"
+  },
+  {
+    "revision": "a3fdc39e66bd292b0300b392e7dbe4f8",
+    "url": "/memory-game/static/media/ruby-original.a3fdc39e.svg"
+  },
+  {
+    "revision": "f704b843540e5985728eafb1bd6f2b19",
+    "url": "/memory-game/static/media/rubymine-original.f704b843.svg"
+  },
+  {
+    "revision": "4cb48ced8c38b08ed325f37f039fd935",
+    "url": "/memory-game/static/media/safari-original.4cb48ced.svg"
+  },
+  {
+    "revision": "88c1047792044b547d1d3e066d3e18c4",
+    "url": "/memory-game/static/media/sass-original.88c10477.svg"
+  },
+  {
+    "revision": "b8a803afc78c1e8a9a8edd77cd2b5c0f",
+    "url": "/memory-game/static/media/sequelize-original.b8a803af.svg"
+  },
+  {
+    "revision": "9be5eb4a1357dff3f5f77bebf389fbef",
+    "url": "/memory-game/static/media/sketch-original.9be5eb4a.svg"
+  },
+  {
+    "revision": "bd4a3d9256994fb686702e51ebec4816",
+    "url": "/memory-game/static/media/slack-original.bd4a3d92.svg"
+  },
+  {
+    "revision": "b1c123df858207acbfa4332d0da080a1",
+    "url": "/memory-game/static/media/sourcetree-original.b1c123df.svg"
+  },
+  {
+    "revision": "7b0b128602429c4bca602fa929db0cd7",
+    "url": "/memory-game/static/media/ssh-original.7b0b1286.svg"
+  },
+  {
+    "revision": "e68c487be93133788f116498db61df7a",
+    "url": "/memory-game/static/media/stylus-original.e68c487b.svg"
+  },
+  {
+    "revision": "3f36cb0243e3952dae2675338749991c",
+    "url": "/memory-game/static/media/swift-original.3f36cb02.svg"
+  },
+  {
+    "revision": "6d5db8b4222fa6748849490420f81021",
+    "url": "/memory-game/static/media/symfony-original.6d5db8b4.svg"
+  },
+  {
+    "revision": "3804338a43b365b38d140a9c585d2b97",
+    "url": "/memory-game/static/media/tomcat-original.3804338a.svg"
+  },
+  {
+    "revision": "54b8b9ddb9acbae94ad0e3d13adb4e2d",
+    "url": "/memory-game/static/media/twitter-original.54b8b9dd.svg"
+  },
+  {
+    "revision": "02867dbe5f804e4dbd0db076bb271cc4",
+    "url": "/memory-game/static/media/typescript-original.02867dbe.svg"
+  },
+  {
+    "revision": "d2be82794cda1e40e7b53b56ea99fd17",
+    "url": "/memory-game/static/media/vagrant-original.d2be8279.svg"
+  },
+  {
+    "revision": "adbb6ec992c2c60344794f9148fe205e",
+    "url": "/memory-game/static/media/vim-original.adbb6ec9.svg"
+  },
+  {
+    "revision": "9cf6b38f4a8f45104d392f15c308e39e",
+    "url": "/memory-game/static/media/vuejs-original.9cf6b38f.svg"
+  },
+  {
+    "revision": "9a7a86a89b95d03d85686fad1578790e",
+    "url": "/memory-game/static/media/webpack-original.9a7a86a8.svg"
+  },
+  {
+    "revision": "29763866c38a60dd94f46501eb9758f0",
+    "url": "/memory-game/static/media/webstorm-original.29763866.svg"
+  },
+  {
+    "revision": "69aab94726675641ce2112507418491c",
+    "url": "/memory-game/static/media/windows8-original.69aab947.svg"
+  },
+  {
+    "revision": "3d9f99264633d811fd45d86b8fe877f0",
+    "url": "/memory-game/static/media/wordpress-original.3d9f9926.svg"
+  },
+  {
+    "revision": "a75c9899b1e6c8b48ffb7e324f2b1a9d",
+    "url": "/memory-game/static/media/yarn-original.a75c9899.svg"
+  },
+  {
+    "revision": "f810300d346bd50baa7d181417bb90eb",
+    "url": "/memory-game/static/media/yii-original.f810300d.svg"
+  },
+  {
+    "revision": "f2b69f2fb35dd74c6d3b1da7fa0e8597",
+    "url": "/memory-game/static/media/yunohost-original.f2b69f2f.svg"
+  }
+]);
